c62eeb5637f3c37fce8fa59c889c23803ec5f86b
Soldatix <elvis.soldatic@gmail.com>
2026-09-17 22:37:59 +0200

    Extract presets and translations

---
+++ src/data/presets.js
@@ -0,0 +1,9 @@
+export const presets=[
+ {id:'6',label:'6',m:6,e:0,mm:49,em:12},
+ {id:'6+1',label:'6+1',m:6,e:1,mm:49,em:10},
+ {id:'7',label:'7',m:7,e:0,mm:39,em:12},
+ {id:'5+2',label:'5+2',m:5,e:2,mm:50,em:12},
+ {id:'5+1',label:'5+1',m:5,e:1,mm:50,em:12},
+ {id:'6+2',label:'6+2',m:6,e:2,mm:49,em:12},
+ {id:'custom',label:'Custom',m:6,e:0,mm:49,em:12}
+];
